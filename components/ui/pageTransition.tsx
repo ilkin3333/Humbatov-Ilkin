530f568ce4a4a2b7149de4fc6c198941e4c2529f
ilkin3333 <ihumbetov09@gmail.com>
2024-06-19 17:39:24 +0400

    problems solved

--- components/ui/pageTransition.tsx
+++ components/ui/pageTransition.tsx
@@ -2,28 +2,21 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 
-const pageTransition = ({ children }) => {
+const PageTransition = ({ children }) => {
   const pathname = usePathname();
 
   return (
-
-
     <AnimatePresence>
-      <div key ={pathname}>
       <motion.div
-    
+        key={pathname}
         initial={{ opacity: 1 }}
-        animate={{ 
-          opacity: 0,
-        transition:{delay:1, duration: 0.4, ease :"easeInOut"},
-        }}
+        animate={{ opacity: 0, transition: { delay: 1, duration: 0.4, ease: "easeInOut" } }}
+        exit={{ opacity: 0, transition: { duration: 0.4, ease: "easeInOut" } }}
         className="h-screen w-screen fixed bg-primary top-0 pointer-events-none"
-  
       />
-        {children}
-        </div>
+      {children}
     </AnimatePresence>
   );
 };
 
-export default pageTransition;
+export default PageTransition;
