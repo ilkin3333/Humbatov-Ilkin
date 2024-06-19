0df7940fc3bb4e6bc5497b1f3a7c1b0984952caa
ilkin3333 <ihumbetov09@gmail.com>
2024-06-19 18:38:33 +0400

    page.transition problems solved

--- components/ui/pageTransition.tsx
+++ components/ui/pageTransition.tsx
@@ -1,10 +1,9 @@
 "use client";
 import { AnimatePresence, motion } from "framer-motion";
 import { usePathname } from "next/navigation";
+import { ReactNode } from "react";
 
-
-
-const PageTransition = ({ children }) => {
+const PageTransition = ({ children }: { children: ReactNode }) => {
   const pathname = usePathname();
 
   return (
